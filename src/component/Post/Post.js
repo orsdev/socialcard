e4f379a388636f07c34819951081976071eb2a8f
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-26 11:05:30 +0100

    Post functional component created

--- src/component/Post/Post.js
+++ src/component/Post/Post.js
@@ -3,24 +3,23 @@ import User from '../../img/user.jpg';
 
 const Posts = (props) => {
  return (
-  <div class="container posts p-0">
-   <div class="posts__bg">
-   <span class="posts__picture"> &nbsp; <strong>@Doe</strong></span>
-    <h2 class="posts__heading text-light text-center"> Learning React? Start Small.</h2>
-    <div class="user">
-     <span class="user__name">John Doe</span>
-     <span class="user__pic">
+  <div className="container posts p-0">
+   <div className="posts__bg">
+   <span className="posts__picture"> &nbsp; <strong>@Doe</strong></span>
+    <h2 className="posts__heading text-light text-center"> Learning React? Start Small.</h2>
+    <div className="user">
+     <span className="user__name">John Doe</span>
+     <span className="user__pic">
       <img src={User} alt="john doe"/>
      </span>
     </div>
    </div>
-   <div class="posts__preview p-2">
-    <h4 class="posts__title">Learning React? Start Small.</h4>
-    <p>Can't pry yourself away from the tutorials> The cure is to make tiny little experiment apps.</p>
-    <p class="link">dev.to</p>
+   <div className="posts__preview p-2">
+    <h4 className="posts__title">Learning React? Start Small.</h4>
+    <p>Can't pry yourself away from the tutorials. The cure is to make tiny little experiment apps.</p>
+    <p className="link">dev.to</p>
    </div>
   </div>
-
  )
 }
 
