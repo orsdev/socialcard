83386d3c2c0e0e3ac92158fc73e3f2b35c89cd86
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-26 11:04:12 +0100

    Info functional component created

--- src/component/Info/Info.js
+++ src/component/Info/Info.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Info = (props) => {
  let info = (
-  <div class="container info__container">
-    <span class="picture text-light"> &nbsp; <strong>@Doe</strong></span>
-    <div class="header__title">
+  <div className="container info__container">
+    <span className="picture text-light"> &nbsp; <strong>@Doe</strong></span>
+    <div className="header__title">
      <h5>The Practical Dev</h5>
      <p> Learning React? Start Small</p>
-     <span class="small"> author: <code class="text-primary">@dceddia</code></span>
+     <span className="small"> author: <code className="text-primary">@dceddia</code></span>
     </div>
   </div>
  );
