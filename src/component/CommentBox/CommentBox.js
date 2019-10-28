3048c1442fd0c9640d7b7ff8655833c514bc5741
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-28 14:24:17 +0100

    commentBox functional component added

---
+++ src/component/CommentBox/CommentBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentBox = (props) => {
+ return (
+ <div class='comment_box'>
+   <textarea name="commentbox" id="commentbox" class="commentbox" cols="30" rows="5"></textarea>
+ </div>
+ );
+};
+
+export default CommentBox;
