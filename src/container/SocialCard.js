3e5d321bd3388c03813f9a13339f72982d2d5719
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-28 14:27:42 +0100

    commentBox method passed to Engagement component

--- src/container/SocialCard.js
+++ src/container/SocialCard.js
@@ -3,12 +3,14 @@ import Layout from '../component/Layout/Layout';
 import Info from '../component/Info/Info';
 import Post from '../component/Post/Post';
 import Engagement from '../component/Engagement/Engagement';
+import CommentBox from '../component/CommentBox/CommentBox';
 
 class SocialCard extends Component {
 
  state = {
   toggleLike: false,
   toggleRetweet: false,
+  toggleCommentBox: false
  };
 
  likeToggle = () => {
@@ -25,10 +27,20 @@ class SocialCard extends Component {
   })
  }
 
+ CommentBox = () => {
+  let toggle = !this.state.toggleCommentBox;
+  this.setState({
+   toggleCommentBox: toggle
+  })
+ }
+
+ compo
+
  render() {
 //declare variables
 let like_count;
 let retweet_count;
+let commentbox = null;
 
 //it true, assign variable a value
 if(this.state.toggleLike){
@@ -39,6 +51,11 @@ if(this.state.toggleLike){
 if(this.state.toggleRetweet){
  retweet_count = 1;
 }
+
+if(this.state.toggleCommentBox){
+ commentbox = <CommentBox />
+}
+
   return (
    <Layout>
     <div className="social__card">
@@ -50,7 +67,9 @@ if(this.state.toggleRetweet){
       retweetCount= {retweet_count}
       retweet={this.state.toggleRetweet}
       liked={this.likeToggle}
-      retweeted={this.retweetToggle} />
+      retweeted={this.retweetToggle} 
+      showTextarea={this.CommentBox}/>
+      {commentbox}
     </div>
    </Layout>
   )
