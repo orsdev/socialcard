8aa6be4587b3957f27d67438d6187c0edc0fb61c
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-28 13:08:36 +0100

    tweet_count and like_count added to SocialCard class component

--- src/container/SocialCard.js
+++ src/container/SocialCard.js
@@ -2,14 +2,55 @@ import React, { Component } from 'react';
 import Layout from '../component/Layout/Layout';
 import Info from '../component/Info/Info';
 import Post from '../component/Post/Post';
+import Engagement from '../component/Engagement/Engagement';
 
 class SocialCard extends Component {
+
+ state = {
+  toggleLike: false,
+  toggleRetweet: false,
+ };
+
+ likeToggle = () => {
+  let toggle = !this.state.toggleLike;
+  this.setState({
+   toggleLike: toggle
+  })
+ }
+
+ retweetToggle = () => {
+  let toggle = !this.state.toggleRetweet;
+  this.setState({
+   toggleRetweet: toggle
+  })
+ }
+
  render() {
+//declare variables
+let like_count;
+let retweet_count;
+
+//it true, assign variable a value
+if(this.state.toggleLike){
+ like_count = 1;
+}
+
+//it true, assign variable a value
+if(this.state.toggleRetweet){
+ retweet_count = 1;
+}
   return (
    <Layout>
-    <div class="social__card">
+    <div className="social__card">
      <Info />
      <Post />
+     <Engagement
+      like={this.state.toggleLike}
+      likeCount= {like_count}
+      retweetCount= {retweet_count}
+      retweet={this.state.toggleRetweet}
+      liked={this.likeToggle}
+      retweeted={this.retweetToggle} />
     </div>
    </Layout>
   )
