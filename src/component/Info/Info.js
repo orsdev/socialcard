4584ab3f9e5d73f459cdbb14d3777c88fdd594f8
horlumiday <oluwadareysamuel@yahoo.com>
2019-11-13 12:10:37 +0100

    username changed, user picture also change

--- src/component/Info/Info.js
+++ src/component/Info/Info.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Info = (props) => {
  let info = (
   <div className="container info__container">
-    <span className="picture text-light"> &nbsp; <strong>@Doe</strong></span>
+    <span className="picture text-light"> &nbsp; <strong>@Sam</strong></span>
     <div className="header__title">
      <h5>The Practical Dev</h5>
      <p> Learning React? Start Small</p>
-     <span className="small"> author: <code className="text-primary">John Doe</code></span>
+     <span className="small"> author: <code className="text-primary">Samuel</code></span>
     </div>
   </div>
  );
--- src/component/Post/Post.js
+++ src/component/Post/Post.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import User from '../../img/user.jpg';
+import User from '../../img/ogundare.jpg';
 
 const Posts = (props) => {
  return (
   <div className="container posts p-0">
    <div className="posts__bg">
-   <span className="posts__picture"> &nbsp; <strong>@Doe</strong></span>
+   <span className="posts__picture"> &nbsp; <strong>@Sam</strong></span>
     <h2 className="posts__heading text-light text-center"> Learning React? Start Small.</h2>
     <div className="user">
-     <span className="user__name">John Doe</span>
+     <span className="user__name">Samuel</span>
      <span className="user__pic">
       <img src={User} alt="john doe"/>
      </span>
@@ -17,7 +17,11 @@ const Posts = (props) => {
    <div className="posts__preview p-2">
     <h4 className="posts__title">Learning React? Start Small.</h4>
     <p>Can't pry yourself away from the tutorials. The cure is to make tiny little experiment apps.</p>
-    <p className="link">dev.to</p>
+    <p className="link">
+     <code class="badge badge-danger p-1">
+      <a class="text-light" target="_blank" href="https://github.com/orsdev">github</a>
+      </code>
+      </p>
    </div>
   </div>
  )
