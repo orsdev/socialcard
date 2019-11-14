7f1800ea4d36c41c07d455acab8c671fc113fb5f
horlumiday <oluwadareysamuel@yahoo.com>
2019-11-14 14:05:47 +0100

    Updates

---
+++ precache-manifest.23c0b944a96f4bef5e8215d57ab07776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063cf2e5f892273eed8e4f49cf7afa4b",
+    "url": "/socialcard/index.html"
+  },
+  {
+    "revision": "1c6b3a24eb682b529e8a",
+    "url": "/socialcard/static/css/main.dd32c054.chunk.css"
+  },
+  {
+    "revision": "80e25e42b226433d6b1c",
+    "url": "/socialcard/static/js/2.f74707df.chunk.js"
+  },
+  {
+    "revision": "1c6b3a24eb682b529e8a",
+    "url": "/socialcard/static/js/main.47fdff58.chunk.js"
+  },
+  {
+    "revision": "f05dc9ca33269e79efb8",
+    "url": "/socialcard/static/js/runtime-main.4d392897.js"
+  },
+  {
+    "revision": "55eef52fcf24de0aa8fcafd9984af6ed",
+    "url": "/socialcard/static/media/ogundare.55eef52f.jpg"
+  }
+]);
